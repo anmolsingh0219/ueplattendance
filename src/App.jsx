72befb4edd3c6b81a55552880e09468f32a0091f
anmolsingh0219 <anmol.scit20@sot.pdpu.ac.in>
2024-01-31 16:11:07 +0530

    oauth

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 // src/App.jsx
 import { RecoilRoot } from 'recoil';
-import { HomePage } from './pages/HomePage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import OAuthCallback from './components/OAuthCallback'; // Make sure to create this component
 
 const App = () => {
   return (
     <RecoilRoot>
-      <div className="App">
-        <HomePage />
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/auth/google/callback" component={OAuthCallback} />
+            {/* Add other routes as needed */}
+          </Switch>
+        </div>
+      </Router>
     </RecoilRoot>
   );
 };
---
+++ src/pages/components/OAuthCallback.jsx
@@ -0,0 +1,33 @@
+// In OAuthCallback.jsx
+import { useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import axios from 'axios'; // Ensure axios is installed
+
+const OAuthCallback = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  useEffect(() => {
+    const exchangeCodeForToken = async () => {
+      const queryParams = new URLSearchParams(location.search);
+      const code = queryParams.get('code');
+
+      if (code) {
+        try {
+          const response = await axios.post('YOUR_BACKEND_ENDPOINT', { code });
+          localStorage.setItem('accessToken', response.data.accessToken);
+          history.push('/dashboard');
+        } catch (error) {
+          console.error('Error exchanging token', error);
+          history.push('/login');
+        }
+      }
+    };
+
+    exchangeCodeForToken();
+  }, [location, history]);
+
+  return <div>Logging you in...</div>;
+};
+
+export default OAuthCallback;
