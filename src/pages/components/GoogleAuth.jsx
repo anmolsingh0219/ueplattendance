041ca41bd36a7e8a5887b9877ab482f34635b530
anmolsingh0219 <anmol.scit20@sot.pdpu.ac.in>
2024-02-01 15:27:33 +0530

    navigate

--- src/pages/components/GoogleAuth.jsx
+++ src/pages/components/GoogleAuth.jsx
@@ -1,16 +1,19 @@
 // src/components/GoogleAuth.jsx
 import { useGoogleLogin } from '@react-oauth/google';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const GoogleAuth = () => {
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleLogin = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
       console.log(tokenResponse); // Here you can send the tokenResponse.code to your backend if necessary
       localStorage.setItem('access_token', tokenResponse.access_token);
       // Navigate to the dashboard or home page after successful login
-      window.location.href = '/homepage';
+      // window.location.href = '/homepage';
+      navigate('/homepage');
     },
     onError: (error) => {
       console.error('Login Failed:', error);
