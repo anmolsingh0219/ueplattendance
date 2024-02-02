c5da4866d3ae35c0bca7599c2eeedc80a1e1f3a5
anmolsingh0219 <anmol.scit20@sot.pdpu.ac.in>
2024-02-02 15:22:32 +0530

    9

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App.jsx'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // Accessing environment variables in Vite
