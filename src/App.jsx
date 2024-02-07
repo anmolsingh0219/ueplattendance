791faf4b85ebca96b5e2bce64f4f95654464a525
anmolsingh0219 <anmol.scit20@sot.pdpu.ac.in>
2024-02-07 12:38:39 +0530

    import errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // src/App.jsx
 import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import GoogleAuth from './components/GoogleAuth';
-import OAuthCallback from './components/OAuthCallback';
+import GoogleAuth from './pages/components/GoogleAuth';
+import OAuthCallback from './pages/components/OAuthCallback';
 import HomePage from './HomePage';
-import refreshAccessToken from './components/RefreshToken';
+import refreshAccessToken from './pages/components/RefreshToken';
 
 const App = () => {
   useEffect(() => {
