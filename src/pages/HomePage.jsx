3f3b93e0f61f2485110ef5a6305f25d185c787f9
anmolsingh0219 <anmol.sict20@sot.pdpu.ac.in>
2024-02-12 18:00:35 +0530

    Homepage Updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,10 @@ const HomePage = () => {
   return (
     <RecoilRoot>
       <div className="flex flex-col items-center min-h-screen w-full bg-gray-800 text-white p-6">
-        <img src={companyLogo} alt="Company Logo" className="mb-4" />
+        {/* Added a div with a white background and some padding */}
+        <div className="bg-white p-2 rounded">
+          <img src={companyLogo} alt="Company Logo" className="mb-4" />
+        </div>
         <h1 className="text-4xl font-bold mb-8">Attendance Tracker</h1>
         <div className="flex flex-wrap justify-around">
           <div className="rounded">
